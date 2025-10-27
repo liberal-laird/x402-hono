975803297e909f62ee27c1689ae7a10701a86d93
lau <lairdnotehk@gmail.com>
2025-10-27 15:12:31 +0800

    remove facilitator

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,11 @@ import { paymentMiddleware, Network, Resource } from "x402-hono";
 import { facilitator } from "@coinbase/x402";
 config();
 
-const facilitatorUrl = process.env.FACILITATOR_URL as Resource;
+
 const payTo = process.env.ADDRESS as `0x${string}`;
 const network = process.env.NETWORK as Network;
 
-if (!facilitatorUrl || !payTo || !network) {
+if ( !payTo || !network) {
   console.error("Missing required environment variables");
   process.exit(1);
 }
@@ -20,7 +20,6 @@ console.log("Server is running");
 console.log("Configuration:");
 console.log("- Network:", network);
 console.log("- PayTo:", payTo);
-console.log("- Facilitator:", facilitatorUrl);
 
 app.use(
   paymentMiddleware(
